6506b4b5f3143a34feb946a57732c6473c7ccb7f
Daniel <toomey.d.p@gmail.com>
2023-06-16 12:36:38 -0500

    Add button assignment and display to variables
    JS - Implement global variables and assign buttons to variables using queryselector

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 let currentNumber = "";
+let previousNumber = "";
+let operator = "";
+
+const currentDisplayNumber = document.querySelector(".current-num");
+const previousDisplayNumber = document.querySelector(".previous-num");
+
+const equal = document.querySelector(".equal");
+const decimal = document.querySelector(".decimal");
+const clear = document.querySelector(".clear");
+const numberButtons = document.querySelector(".number");
+const operators = document.querySelector(".operator");
